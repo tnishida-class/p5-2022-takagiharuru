16ff371b7c0efc125641bb136c6abe8df73e4101
takagiharuru <2161032h@stu.kobe-u.ac.jp>
2022-10-11 22:37:44 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,21 +5,17 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      if(i+j%2==1){
+      if((i+j)%2==1){
         fill(130);
         rect(size*i,size*j,size,size);// BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
         if(j<3){
           fill(255,0,0);
         }
         else if(j>4){
-          fill(0)
+          fill(0);
         }
         noStroke();
         circle(size*i+size/2,size*j+size/2,size-4)
-      }
-      else{
-        fill(255);
-        rect(size*i,size*j,size,size);
       }
     }
   }

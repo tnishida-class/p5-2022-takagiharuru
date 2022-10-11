0af3e04e528809b344e14c9b9077cb5306426a51
takagiharuru <2161032h@stu.kobe-u.ac.jp>
2022-10-11 11:21:02 +0900

    update1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -3,14 +3,19 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  strokeWeight(4);
-  fill(178, 100, 162);
+  background(255);
+  strokeWeight(0);
+  fill(124, 30, 128);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
   fill(255);
-  textSize(32);
+  triangle(14,112,108,112,108,14);
+  strokeWeight(1.25);
+  stroke(240,153,180);
+  fill(255);
+  triangle(0,108,104,108,104,0);
+  strokeWeight(0);
+  fill(240,153,180);
+  textSize(29);
   textFont("serif");
-  text("46", 68, 100);
+  text("46", 67, 105);
 }
--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(100,100)
+  for(let i = 10; i > 0; i--){
+    if(i<=5){
+      stroke(0,0,255);
+    }
+    else{
+      stroke(255,0,0);
+    }
+    strokeWeight(1.5);
+    ellipse(50,50,i*10)
   }
 }

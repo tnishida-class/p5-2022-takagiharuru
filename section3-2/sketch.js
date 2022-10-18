ce60e5fa09ebab605da2fb2650f8e4748cec5b6c
takagiharuru <2161032h@stu.kobe-u.ac.jp>
2022-10-19 00:41:40 +0900

    change section3-2

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -1,36 +1,37 @@
 // テキスト「アニメーションの基本」
-let x, y, vx, vy;
+let x, y, vy;
 const g = 1; // 重力加速度
-const vyMax = 30;
+const jump = 20;
+const size = 20
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  x = width / 2;
-  y = height / 2;
-  vx = 8;
-  vy = 8;
+  x = width - size / 2;
+  y = height - size / 2;
+  vy = 0;
 }
 
 function draw(){
   background(160, 192, 255);
-  ellipse(x, y, 20, 20);
-  x += vx;
+  ellipse(x, y, size, size);
   y += vy;
 
-  // 重力（コメント機能でオンオフ切り替えて実行してみましょう）
-  vy = constrain(vy + g, -vyMax, vyMax);
+  if(y < height - size / 2){
+    vy += g;
+  }
+  else{
+    vy = 0;
+  }
 
-  // 端の処理パターン (1) 反対側から出てくる
-  // if(x > width){ x = 0; }
-  // else if(x < 0){ x = width; }
-  // if(y > height){ y = 0; }
-  // if(y < 0){ y = height; }
+  if(keyIsDown(LEFT_ARROW)){x -= 5;}
+  if(keyIsDown(RIGHT_ARROW)){x += 5;}
 
-　// 端の処理パターン (2) 跳ね返る
-  if(x < 0 || x > width){ vx = -1 * vx; }
-  if(y > height){ vy = -1 * vy; }
-  x = constrain(x, 0, width);
-  y = constrain(y, 0, height);
+}
+
+function keyPressed(){
+  if(key==" "){
+    vy = -jump;  
+  }
 }
 
 function windowResized(){

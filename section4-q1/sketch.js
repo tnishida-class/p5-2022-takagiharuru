a0649e4c82cedd1ca5fab3b44c4c5b8360d7ed3e
takagiharuru <2161032h@stu.kobe-u.ac.jp>
2022-11-11 10:00:42 +0900

    change section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,14 +1,16 @@
 // テキスト「配列を使った描画」練習問題：折れ線グラフ
+let scores = [];
 
 function setup(){
   createCanvas(400, 400);
-  background(240);
+    // 配列をランダムに初期化する
+    for(let i = 0; i < 10; i++){
+      scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
+    }
+}
 
-  // 配列をランダムに初期化する
-  let scores = [];
-  for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
-  }
+function draw(){
+  background(240);
 
   // 横線を引く
   const n = 10;
@@ -19,6 +21,24 @@ function setup(){
   const dx = width / scores.length;
   let px, py; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
-    // BLANK[1]
+    line(px, py, dx*i+dx/2, height-(scores[i]*4));
+    px = dx*i+dx/2;
+    py = height-(scores[i]*4);
+    // console.log(py);
+    // console.log(height-(scores[i]*4));
+    circle(px, py, 5);
+  }
+
+  for(let i = 0; i < scores.length; i++){
+    let s=round(scores[i]);
+    if(dist(mouseX, mouseY, dx*i+dx/2, height-(scores[i]*4))<10){
+      fill(212, 25, 25);
+      if((height-(scores[i]*4)-5-(textAscent() + textDescent()))<0){
+        text(s, dx*i+dx/2-20, height-(scores[i]*4)+10);
+      }
+      else{
+        text(s, dx*i+dx/2-5, height-(scores[i]*4)-5);
+      }
+    }
   }
 }
